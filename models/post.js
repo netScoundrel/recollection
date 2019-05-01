163aaa422f4680f83a83808eadb2bfb774058a68
Ruben Galoyan <ruben071099@gmail.com>
2019-05-01 11:50:46 +0300

    Post schema added

---
+++ models/post.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+    ownerId:{
+        type: String,
+        unique: false,
+        required: false, //needs to be changed later
+    },
+    title:{
+        type: String,
+        unique: false,
+        required: true,
+    },
+    text:{
+        type: String,
+        unique: false,
+        required: true,
+    },
+    accesibility:{
+        type: String,
+        unique: false,
+        required: false, //needs to be changed later
+    },
+    publishDate:{
+        type: Date,
+        unique: false,
+        required: true,
+    },
+    likes:{
+        type: Array,
+        unique: false,
+        required: false,
+    }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+module.exports = Post;
